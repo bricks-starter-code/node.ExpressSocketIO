310f97a79d4532fc37e9f033121662167ce49c45
B Ricks <bricks@unomaha.edu>
2017-11-30 13:29:58 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,32 @@
 var express = require('express');
 var path = require('path');
 var app = express();
-var http = require('http').Server(app);
+var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
 var port = 3000;
+
 
 app.use(express.static(path.join(__dirname, 'static')));
 
 
 io.on('connection', function(socket){
   console.log('a user connected');
+  
+  socket.on('join', function(msg){
+    
+    console.log("join request");
+    socket.emit('joinaccept', '');
+    
+  });
+  
+  socket.on('disconnect', function(){
+    console.log("disconnected");
+  })
 });
 
+
 // Listen for requests
-var server = app.listen(port, function() {
+var server = http.listen(port, function() {
   console.log('Loaded on port: ' + port);
 });
